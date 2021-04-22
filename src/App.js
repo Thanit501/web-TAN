57dcafccbb0ebd24a42d1800cbce51cb2775aebc
Thanit Nookao <Tnookao@gmail.com>
2021-04-22 22:10:03 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../src/index.css";
+import Home from './Demo/Home';
+import Login from './Demo/Login';
+import Signup from './Demo/Signup';
+import Adduser from './Demo/Adduser';
+import Navbar from './Demo/Navbar';
+import Dashborad from './Demo/Dashborad';
+import urlnotfound from './Demo/urlnotfound';
+import { Switch, Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    <>
+    <Switch>
+        <Route  exact  path="/" component={Home} />
+        <Route  exact  path="/Login" component={Login} />
+        <Route  exact  path="/Signup" component={Signup} />
+        <Route  exact  path="/Adduser" component={Adduser} /> 
+        <Route  exact  path="/Dashborad" component={Dashborad} />
+        <Route  component= {urlnotfound} />
+        <Redirect to="/" />  
+    </Switch>
+    
+    </>
+  )
+};
 export default App;
